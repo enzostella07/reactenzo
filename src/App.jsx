c1c4b868430244f3d747fe0c03d8491c7fbeebeb
Ramiro <ramifazio@gmail.com>
2022-11-03 10:29:16 -0300

    Sugerencias!

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import './App.css';
-import ItemListContainer from './components/Itemlistcontainer';
-import Navmui from './components/Navbar';
-
+import React from "react"; //! Archivos .jsx siempre va la importacion de react
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./App.css";
+import ItemListContainer from "./components/Itemlistcontainer";
+import Navmui from "./components/Navbar";
 
 export default function App() {
   return (
     <div>
-      <Navmui/>
-      <ItemListContainer greeting={true} color = {"#00FF00"} />
+      <Navmui />
+      <ItemListContainer greeting={true} color={"#00FF00"} />
     </div>
   );
 }
-
